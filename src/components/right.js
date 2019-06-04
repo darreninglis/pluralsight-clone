98cc8ebe986a5f96ee8ce39c5afe20f2aeea228f
Darren Inglis <darren.inglis@gmail.com>
2019-06-04 13:32:21 +1000

    complete

---
+++ src/components/right.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Right = () => {
+	return (
+		<div id='right'>
+			<div id='showcase'>
+				<div className='showcase-content'>
+					<h1 className='showcase-text'>
+						Let's create the future <strong>together</strong>
+					</h1>
+					<a href='#' className='secondary-btn'>
+						Start a FREE 10-day trial
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Right;
